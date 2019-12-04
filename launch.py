ffb6e226f6c3bb0c07c5177ebd0c5f3f6a93bf58
anwarvic <mohamedanwarvic@gmail.com>
2019-12-04 21:33:49 +0200

    deleted unnecessary code from launch.py... didn't change any code

--- launch.py
+++ launch.py
@@ -32,37 +32,5 @@ def call_chatbot():
 
 
 
-# @app.route('/', methods=['POST'])
-# def my_form_post():
-# 	text = request.form['search_query']
-# 	return redirect(url_for('elastic_query', query_text=text))
-
-
-# @app.route('/query/<query_text>')
-# def elastic_query(query_text):
-# 	hits = es_search(ES, query_text)
-# 	query_out = [hit['_source'] for hit in hits]
-# 	res = render_template('result.html', 
-# 						  query_text=query_text,
-# 						  toPass=query_out)
-# 	return res
-
-
-
-
-# @app.route('/movie/<id_>')
-# def chart(id_):
-# 	hits = es_search(ES, id_)
-# 	out = hits[0]['_source']
-# 	return render_template('dashboard.html',
-# 							movie_name = out['movie_name'],
-# 							tones=out['average_tones'],
-# 							tweets = out['tweets'])
-
-
-
-
-
-
 if __name__ == '__main__':
-	app.run(host="0.0.0.0", debug = True, port=5000)
+	app.run(host="0.0.0.0", debug=True, port=5000)

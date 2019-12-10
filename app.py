58bcf663560dd16fb05e06c5903404b439cd516d
anwarvic <mohamedanwarvic@gmail.com>
2019-12-10 12:19:47 +0200

    handled the case when the audio data is empty

--- app.py
+++ app.py
@@ -46,20 +46,22 @@ def call_chatbot():
 @app.route('/send_audio_msg', methods=['POST'])
 def call_asr():
 	req = flask.request.data
-	
-	bytes_stream = b64decode(req.split(b',')[1])
-	audio = AudioSegment.from_file(BytesIO(bytes_stream))
-	# convert it to numpy
-	data = np.frombuffer(audio._data, dtype=np.int32)
-	# change type to np.int16 by dropping bottom 16 bits
-	# data = (data>>16).astype(np.int16)
-	# change type to np.float32
-	data = data.astype('float32') / np.iinfo(np.int32).max  # normalize audio
-	# change sample rate from 48000 to 16000
-	data = librosa.core.resample(data, 48000, 16000)
-	# transcribe the provided data
-	out = asr_model.transcribe(data)
-
+	header, *bytes_stream = req.split(b',')
+	if bytes_stream:
+		bytes_stream = b64decode(bytes_stream[0])
+		audio = AudioSegment.from_file(BytesIO(bytes_stream))
+		# convert it to numpy
+		data = np.frombuffer(audio._data, dtype=np.int32)
+		# change type to np.int16 by dropping bottom 16 bits
+		# data = (data>>16).astype(np.int16)
+		# change type to np.float32
+		data = data.astype('float32') / np.iinfo(np.int32).max  # normalize audio
+		# change sample rate from 48000 to 16000
+		data = librosa.core.resample(data, 48000, 16000)
+		# transcribe the provided data
+		out = asr_model.transcribe(data)
+	else:
+		out = " "
 	# form response
 	flask_response = app.response_class(response=flask.json.dumps({"text": out}),
 										status=200,

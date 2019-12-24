8cd05275998871e7f316576fc5aadc1ec8d840ad
anwarvic <mohamedanwarvic@gmail.com>
2019-12-24 12:46:22 +0200

    adding function to normalize audio

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import yaml
+import numpy as np
 
 
 
@@ -16,4 +17,45 @@ def parse_yaml(filepath="conf.yaml"):
         except Exception as exc:
             print("ERROR while parsing YAML conf.")
             return exc
+
+
+def normalize_audio(audio, method="z-score"):
+    """
+    This method is used to normalize/scale the audio based on different 
+    normalization methods. Normalization is important for the ASR model to treat
+    loud audio as good as quite audio
+    Args:
+        audio (numpy.array): the audio data as 1D numpy array
+        method (str): normalization method.. default (z-score)
+    Returns:
+        numpy.array of the audio data after normalization
+    NOTE:
+    "-1_1" normalization is a special case of min-max normalization where the
+    min is -1 and the max is 1
+    SEE:
+    https://en.wikipedia.org/wiki/Normalization
+    """
+    norm_methods = ["-1_1", "z-score", "mean"]
+    assert method in norm_methods,\
+            "normalization method must be one of these " + norm_methods
+    if method == "-1_1":
+        # apply 0-1 normalization method
+        new_min, new_max = -1, 1
+        old_min, old_max = audio.min(), audio.max()
+        scaled = (audio-old_min)/(old_max-old_min) * (new_max-new_min) + new_min
+    elif method == "mean":
+        # apply mean normalization
+        meu = np.mean(audio)
+        mn, mx = audio.min(), audio.max()
+        scaled = (audio - meu) / (mx - mn)
+    elif method == "z-score":
+        # apply z-score normalization (standarization)
+        std = np.std(audio)
+        meu = np.mean(audio)
+        scaled = (audio - meu) / std
+    # get back the scaled version
+    return scaled
+
+
+
 
